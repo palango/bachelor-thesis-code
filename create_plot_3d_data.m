52ae6c2a0e9bf568c1ba51d6bfcf53206083668c
Paul Lange <palango@gmx.de>
2013-09-23 22:27:35 +0200

    add utility for writing 3d plot data for latex

---
+++ create_plot_3d_data.m
@@ -0,0 +1,16 @@
+function [] = create_plot_3d_data(X, Y, Z)
+
+file = fopen('output.txt','w');
+for I=1:length(X)
+  for J=1:length(Y)
+    fprintf(file, '%f %f %f\n', X(I), Y(J), Z(I,J));
+  end
+  fprintf(file, '\n');
+end
+fclose(file);
+
+data = contour(X,Y,Z, [.1, .3, .5, .7, .9]);
+data = data';
+save 'contour.txt' data
+
+end
