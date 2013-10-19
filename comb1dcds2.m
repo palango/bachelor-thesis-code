facd2d17f457550e32a95bf2035813b26834dc75
Paul Lange <palango@gmx.de>
2013-10-19 13:21:29 +0200

    CDS orthogonal

---
+++ comb1dcds2.m
@@ -0,0 +1,191 @@
+clc
+clear all
+close all
+
+SOL=@(x) sin(pi*x)+1;
+MSOL=@(x) pi*cos(pi*x)-pi^2*sin(pi*x);
+
+%SOL=@(x) cos(pi*x)-1;
+%MSOL=@(x) -pi*sin(pi*x)-pi^2*cos(pi*x);
+
+DIF=1.0;
+KONV=1.0;
+BETA=0.5;
+XMIN=0.0;
+XMAX=1.0;
+
+N=40; % KV's
+
+X = linspace(XMIN, XMAX, N+1);
+XC = (X(1:N)+X(2:N+1))/2;
+XCR = [XMIN, XC, XMAX];
+
+%%% ANALYTISCHE LÖSUNG
+TA = zeros(1, N);
+for I=1:N
+  TA(I)=SOL(XC(I));
+end
+
+figure(1)
+plot(XC, TA, 'x-');
+title('Analytische Loesung')
+
+%%% FVM Lösung
+
+% Randbedingungen
+RBE = SOL(XMAX);
+RBW = SOL(XMIN);
+
+% Koeffizienten speichern
+AP = zeros(1, N);
+AE = zeros(1, N);
+AW = zeros(1, N);
+
+for I=1:N
+  DX = X(I+1)-X(I);
+
+  DXE = XCR(I+2)-XCR(I+1);
+  DXW = XCR(I+1)-XCR(I);
+
+  AE(I) = DIF/(DXE);
+  AW(I) = DIF/(DXW);
+
+  AP(I) = -AE(I)-AW(I);
+
+  DCDSE = (X(I+1)-XCR(I+1))/DXE;
+  DCDSW = (XCR(I+1)-X(I))/DXW;
+
+  AEK(I) = KONV * DCDSE;
+  AWK(I) = -KONV * DCDSW;
+
+  APK(I) = KONV * ((1-DCDSE)-(1-DCDSW));
+  AK(I) = KONV;
+end
+
+% Gesamtgleichungssystem aufstellen
+A = zeros(N);
+b = zeros(N,1);
+
+for I=1:N
+  b(I) = MSOL(XC(I))*(X(I+1)-X(I));
+
+  % Hauptdiagonale
+  A(I,I) = AP(I) + APK(I);
+
+  % Westliche Nebendiagonale
+  if mod(I,N)==1
+    b(I) = b(I) - AW(I)*RBW + AK(I)*RBW;
+  else
+    A(I, I-1) = AW(I) + AWK(I);
+  end
+
+  % Östliche Nebendiagonale
+  if mod(I,N)==0
+    b(I) = b(I) - AE(I)*RBE - AK(I)*RBE;
+  else
+    A(I, I+1) = AE(I) + AEK(I);
+  end
+end
+
+T=A\b;
+
+figure(2)
+plot(XC, T, 'x-');
+title('Numerische Loesung')
+
+%%% Lösungsfehler berechnen
+SERR=0.0;
+ERR=zeros(1, N);
+for I=1:N
+    ERR(I)=T(I)-TA(I);
+    SERR=SERR+ERR(I)^2;
+end
+SERR=sqrt(SERR/(N));
+
+figure(3)
+plot(XC, ERR, 'x-');
+title('Loesungsfehler')
+
+fprintf('Summierter Fehler %16.10e N=%g\n', SERR, N);
+
+
+%%% ORDNUNG  BESTIMMEN
+ERR5=2.4182329465e-02;
+ERR10=5.9638142082e-03;
+ERR20=1.4859620816e-03;
+ERR40=3.7118037236e-04;
+op=log((ERR5)/(ERR10))/log(2);
+fprintf('Ordnung des Verfahrens %16.10e \n',op  );
+op=log((ERR10)/(ERR20))/log(2);
+fprintf('Ordnung des Verfahrens %16.10e \n',op  );
+op=log((ERR20)/(ERR40))/log(2);
+fprintf('Ordnung des Verfahrens %16.10e \n',op  );
+
+
+%%% RESIDUUM BERECHNEN
+t=zeros(N, 1);
+for I=1:N
+    t(I)=SOL(XC(I));
+end
+
+RES=A*t-b;
+
+figure(4)
+plot(XC,RES,'x-');
+
+xlabel('XC')
+ylabel('RES')
+title('Residuum')
+
+%%% Truncation Error berechnen
+
+% b ohne Randwerte
+TE=zeros(1, N);
+for I=1:N
+  b(I) = MSOL(XC(I));
+end
+
+for I=3:N-2
+  DX = X(I+1)-X(I);
+  TE(I) = (DX/24 * (b(I+1) - 2*b(I) + b(I-1)))... % TE_source
+        + ((T(I+2)-3*T(I+1)+3*T(I)-T(I-1))/(24*DX))... % TE_e
+        - ((T(I+1)-3*T(I)+3*T(I-1)-T(I-2))/(24*DX))... % TE_w
+        + (T(I+1)-2*T(I)+T(I-1))/8 ... % TE_e_cds
+        + (T(I+2)-2*T(I+1)+2*T(I-1)-T(I-2))/32 ... % TE_e_cds
+        - (T(I+1)-2*T(I)+T(I-1))/8 ...
+        + (T(I+2)-2*T(I+1)+2*T(I-1)-T(I-2))/32;
+  TE(I) = TE(I);
+end;
+
+% TE Sonderfälle für Randvolumen
+I=2;
+DX = X(I+1)-X(I);
+TE(I) = (DX/24 * (b(I+1) - 2*b(I) + b(I-1)))... % TE_source
+      + ((T(I+2)-3*T(I+1)+3*T(I)-T(I-1))/(24*DX))... % TE_e
+      - ((T(I+1)-3*T(I)+4*T(I-1)-2*RBW)/(24*DX)) ...
+      + (T(I+1)-2*T(I)+T(I-1))/8 ... % TE_e_cds
+      + (T(I+2)-2*T(I+1)+2*T(I-1)-2*RBW)/32 ... % TE_e_cds
+      - (T(I+1)-2*T(I)+T(I-1))/8 ...
+      + (T(I+2)-2*T(I+1)+3*T(I-1)-2*RBW)/32;
+TE(I) = TE(I);
+
+I = N-1;
+DX = X(I+1)-X(I);
+TE(I) = (DX/24 * (b(I+1) - 2*b(I) + b(I-1)))... % TE_source
+      + ((2*RBE-4*T(I+1)+3*T(I)-T(I-1))/(24*DX))... % TE_e
+      - ((T(I+1)-3*T(I)+3*T(I-1)-T(I-2))/(24*DX))... % TE_w
+      + (T(I+1)-2*T(I)+T(I-1))/8 ... % TE_e_cds
+      + (2*RBE-3*T(I+1)+2*T(I-1)-T(I-2))/32 ... % TE_e_cds
+      - (T(I+1)-2*T(I)+T(I-1))/8 ...
+      + (2*RBE-3*T(I+1)+2*T(I-1)-T(I-2))/32;
+TE(I) = TE(I);
+
+hold on;
+plot(XC, TE, 'rx-');
+
+RESTE = RES-TE';
+
+figure(5)
+plot(XC(3:N-2), RESTE(3:N-2), 'x-')
+xlabel('XC')
+ylabel('RES-TE')
--- comb1dcds_orth.m
+++ comb1dcds_orth.m
@@ -2,40 +2,24 @@ clc
 clear all
 close all
 
-%SOL=@(x) sin(pi*x)+1;
-%MSOL=@(x)-pi^2*sin(pi*x);
+SOL=@(x) sin(pi*x)+1;
+MSOL=@(x) pi*cos(pi*x)-pi^2*sin(pi*x);
 
-SOL=@(x) cos(pi*x)-1;
-MSOL=@(x)-pi^2*cos(pi*x);
+%SOL=@(x) cos(pi*x)-1;
+%MSOL=@(x) -pi*sin(pi*x)-pi^2*cos(pi*x);
 
 DIF=1.0;
+KONV=1.0;
 ALPHA=0.9;
 XMIN=0.0;
 XMAX=1.0;
-N=80;% KV's
 
-% Immer feiner werdendes Gitter nach Lehrbuch
+N=50; % KV's
+
 X = zeros(1,N+1);
 for I=1:N+1
   X(I) = XMIN + (ALPHA^(I-1)-1)/(ALPHA^N-1)*(XMAX-XMIN);
 end
-
-% N halbieren
-for j=1:1 % jeder Durchlauf entfernt Hälfte der Punkte
-  fprintf('abc');
-
-idx =1;
-X2=0;
-for i=1:N+1
-  if mod(i, 2)==1
-    X2(idx)=X(i);
-    idx=idx+1;
-  end
-end
-X=X2;
-N=length(X)-1;
-end
-
 XC = (X(1:N)+X(2:N+1))/2;
 XCR = [XMIN, XC, XMAX];
 
@@ -61,13 +45,24 @@ AE = zeros(1, N);
 AW = zeros(1, N);
 
 for I=1:N
+  DX = X(I+1)-X(I);
+
   DXE = XCR(I+2)-XCR(I+1);
   DXW = XCR(I+1)-XCR(I);
 
-  AE(I) = DIF/(DXE);
-  AW(I) = DIF/(DXW);
+  AE(I) = DIF/DXE;
+  AW(I) = DIF/DXW;
 
   AP(I) = -AE(I)-AW(I);
+
+  DCDSE = (X(I+1)-XCR(I+1))/DXE;
+  DCDSW = (XCR(I+1)-X(I))/DXW;
+
+  AEK(I) = KONV * DCDSE;
+  AWK(I) = -KONV * DCDSW;
+
+  APK(I) = KONV * ((1-DCDSE)-(1-DCDSW));
+  AK(I) = KONV;
 end
 
 % Gesamtgleichungssystem aufstellen
@@ -78,20 +73,20 @@ for I=1:N
   b(I) = MSOL(XC(I))*(X(I+1)-X(I));
 
   % Hauptdiagonale
-  A(I,I) = AP(I);
+  A(I,I) = AP(I) + APK(I);
 
   % Westliche Nebendiagonale
   if mod(I,N)==1
-    b(I) = b(I)-AW(I)*RBW;
+    b(I) = b(I) - AW(I)*RBW + AK(I)*RBW;
   else
-    A(I, I-1) = AW(I);
+    A(I, I-1) = AW(I) + AWK(I);
   end
 
   % Östliche Nebendiagonale
   if mod(I,N)==0
-    b(I) = b(I)-AE(I)*RBE;
+    b(I) = b(I) - AE(I)*RBE - AK(I)*RBE;
   else
-    A(I, I+1) = AE(I);
+    A(I, I+1) = AE(I) + AEK(I);
   end
 end
 
@@ -114,42 +109,23 @@ figure(3)
 plot(XC, ERR, 'x-');
 title('Loesungsfehler')
 
-fprintf('Summierter Fehler %16.10e N=%g\n', SERR, length(X));
+fprintf('Summierter Fehler %16.10e N=%g\n', SERR, N);
 
 
-%%%% ORDNUNG  BESTIMMEN
-%ERR5=2.4501722712e-02;
-%ERR10=6.4258655119e-03;
-%ERR20=2.0705369759e-03;
-%ERR40=1.0396550077e-03;
-%ERR80=7.0445840647e-04;
-
-% ALPHA=0.99, cos(x)-2
-%ERR5=3.3375433977e-02;
-%ERR10=8.2565958126e-03;
-%ERR20=2.0862964006e-03;
-%ERR40=5.4989893414e-04;
-%ERR80=1.6421419188e-04;
-
-% für verdünntes Gitter
-ERR5=1.8313148130e-01;
-ERR10=1.1754515384e-01;
-ERR20=3.5245466348e-02;
-ERR40=8.8115116752e-03;
-ERR80=2.1893954570e-03;
-
-
+%%% ORDNUNG  BESTIMMEN
+ERR5=2.4182329465e-02;
+ERR10=5.9638142082e-03;
+ERR20=1.4859620816e-03;
+ERR40=3.7118037236e-04;
 op=log((ERR5)/(ERR10))/log(2);
 fprintf('Ordnung des Verfahrens %16.10e \n',op  );
 op=log((ERR10)/(ERR20))/log(2);
 fprintf('Ordnung des Verfahrens %16.10e \n',op  );
 op=log((ERR20)/(ERR40))/log(2);
 fprintf('Ordnung des Verfahrens %16.10e \n',op  );
-op=log((ERR40)/(ERR80))/log(2);
-fprintf('Ordnung des Verfahrens %16.10e \n',op  );
 
 
-%%%% RESIDUUM BERECHNEN
+%%% RESIDUUM BERECHNEN
 t=zeros(N, 1);
 for I=1:N
     t(I)=SOL(XC(I));
@@ -164,7 +140,7 @@ xlabel('XC')
 ylabel('RES')
 title('Residuum')
 
-%%%% Truncation Error berechnen
+%%% Truncation Error berechnen
 
 % b ohne Randwerte
 TERR=zeros(1, N);
@@ -211,14 +187,26 @@ for I=3:N-2
       + (1/(Xe-Xw)*((TE-TP)/(XE-XP)-(TP-TW)/(XP-XW)) - 1/(Xw-Xww)*((TP-TW)/(XP-XW)-(TW-TWW)/(XW-XWW)))...
       * 1/(6*(XP-XW))*(((XW-Xw)^3-(XP-Xw)^3)/(XP-XW));
 
+  TERRKE = 1/(2*(Xe-Xw))*((TE-TP)/(XE-XP) - (TP-TW)/(XP-XW))*(Xe-XE)*(Xe-XP)...
+      + 1/6*(Xe-XP)/DX*((Xe-XP)^2-(XE-XP)^2)...
+      * (1/(XE-XP)*((TEE-TP)/(XEE-XP)-(TE-TW)/(XE-XW)) - 1/(XP-XW)*((TE-TW)/(XE-XW) - (TP-TWW)/(XP-XWW)));
+
+  TERRKW = 1/(2*(Xe-Xw))*((TE-TP)/(XE-XP) - (TP-TW)/(XP-XW))*(Xw-XW)*(Xw-XP)...
+      + 1/6*(Xw-XP)/DX*((Xw-XP)^2-(XW-XP)^2)...
+      * (1/(XE-XP)*((TEE-TP)/(XEE-XP)-(TE-TW)/(XE-XW)) - 1/(XP-XW)*((TE-TW)/(XE-XW) - (TP-TWW)/(XP-XWW)));
+
   % einzelne Fehler addieren
-  TERR(I) = (TERRS-TERRE+TERRW);
+  TERR(I) = (TERRS-TERRE+TERRW-TERRKE+TERRKW);
 end
+
 
 
 hold on;
 plot(XC, TERR, 'rx-');
 
+RESTE = RES-TERR';
+
 figure(5)
-plot(XC, RES-TERR', 'x-');
-title('RES - TE')
+plot(XC(3:N-2), RESTE(3:N-2), 'x-')
+xlabel('XC')
+ylabel('RES-TE')
