2fa98ebe4bcd4b1be735239d533c20c2ef19cfee
Paul <paul@localhost.localdomain>
2013-11-12 23:49:38 +0100

    more progress

--- iteration.m
+++ iteration.m
@@ -4,18 +4,28 @@ clear all;
 
 
 N=20;
-ITER=100;
+ITER=50;
 
 XHIST = zeros(ITER,N+1);
 TEHIST = zeros(ITER,N);
+RESHIST = zeros(ITER,N);
+THIST = zeros(ITER,N);
 
 % Anfangsgitter
-XI = linspace(0,1,N+1);
+%XI = linspace(0,1,N+1);
+
+XMIN=0.0;
+XMAX=1.0;
+ALPHA=0.9;
+XI = zeros(1,N+1);
+for I=1:N+1
+  XI(I) = XMIN + (ALPHA^(I-1)-1)/(ALPHA^N-1)*(XMAX-XMIN);
+end
 
 for A=1:ITER
   XHIST(A,:) = XI;
   % TE ausrechnen
-  TERRI = dif1d_orth_it(N,XI);
+  [TERRI, RESI, TI] = dif1d_orth_it(N,XI);
 
   ERR=0;
   for I=1:N
@@ -30,6 +40,8 @@ for A=1:ITER
 
   % Werte speichern
   TEHIST(A,:) = TERRI;
+  RESHIST(A,:) =RESI;
+THIST(A,:) = TI;
 end
 
 % Suche bestes Ergebnis im Vergleich zu 1
@@ -38,7 +50,9 @@ hold on;
 X1 = XHIST(1,:);
 XC1 = (X1(1:N)+X1(2:N+1))/2;
 TE1 = TEHIST(1,:);
+RES1 = RESHIST(1,:);
 plot(XC1,TE1,'kx-');
+plot(XC1,RES1,'gx-');
 IDXMIN=1;
 ERRMIN=SERR(1);
 for A=1:ITER
@@ -52,12 +66,12 @@ X2 = XHIST(IDXMIN,:);
 XC2 = (X2(1:N)+X2(2:N+1))/2;
 TE2 = TEHIST(IDXMIN,:);
 plot(XC2,TE2,'bx-');
+plot(XC2,RESHIST(IDXMIN,:),'rx-');
 
 figure(2);
 hold on;
-plot(linspace(1,ITER,ITER), SERR)
-plot(linspace(1,ITER,ITER), ERRMIN*ones(1,ITER),'g-')
-plot(linspace(1,ITER,ITER), SERR(1)*ones(1,ITER),'r-')
-
+plot(1:ITER, SERR)
+plot(1:ITER, ERRMIN*ones(1,ITER),'g-')
+plot(1:ITER, SERR(1)*ones(1,ITER),'r-')
 fprintf('Verbesserung relativ: %4.2f%%\n', abs(ERRMIN-SERR(1))/SERR(1)*100);
 
